d0de9329abe01cc94044c487e5d1a3a347fdf241
Unknown <carolmertz@gmail.com>
2019-02-28 11:20:16 -0500

    I added my entire unity project~ wooooooo

---
+++ Cat Roommate Clone/Assets/TextChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] textStuff;
+
+    
+    public void ChangeText()
+    {
+        int randNum;
+        randNum = Random.Range(0, textStuff.Length);
+
+        text.text = textStuff[randNum];
+    }
+}
